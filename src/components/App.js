770aba403ca166698c2c5fb2589ea5cbe7a0fcbb
Ashton Sham <ash@Ashtons-MacBook-Pro.local>
2020-05-29 01:46:42 -0400

    sec 17 class 222

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
@@ -10,9 +10,9 @@ import Header from "./Header";
 const App = () => {
   return (
     <div>
-      <Header classname="ui container" />
       <BrowserRouter>
         <div>
+          <Header classname="ui container" />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
